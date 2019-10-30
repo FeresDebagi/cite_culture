707477c8f2f316f6ef069d3321903db5efdcba6d
FakharElyes <44549958+FakharElyes@users.noreply.github.com>
2019-10-30 13:55:56 +0100

    maj

---
+++ Integration/CiteDeLaCultureIntegration/src/main/java/Controllers/StatController.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+/**
+ * FXML Controller class
+ *
+ * @author Elyes
+ */
+public class StatController implements Initializable {
+
+    @FXML
+    private Button tfBack;
+    @FXML
+    private Label tflogin;
+    @FXML
+    private ImageView tfphoto;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void Back(ActionEvent event) {
+    }
+    
+    /*public StatController() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        XYDataset dataset = createDataset();
+        JFreeChart chart = createChart(dataset);
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+        chartPanel.setBackground(Color.white);
+        add(chartPanel);
+
+        pack();
+        setTitle("Line chart");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private XYDataset createDataset() {
+
+        XYSeries series = new XYSeries("Année 2019");
+        series.add(1, 567);
+        series.add(2, 612);
+        series.add(3, 800);
+        series.add(4, 980);
+        series.add(5, 1410);
+        series.add(6, 2350);
+
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(series);
+
+        return dataset;
+    }
+
+    private JFreeChart createChart(XYDataset dataset) {
+
+        JFreeChart chart = ChartFactory.createXYLineChart(
+                "Average prix per id_event", 
+                "id_event", 
+                "Prix(€)", 
+                dataset, 
+                PlotOrientation.VERTICAL,
+                true, 
+                true, 
+                false 
+        );
+
+        XYPlot plot = chart.getXYPlot();
+
+        XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+        renderer.setSeriesPaint(0, Color.RED);
+        renderer.setSeriesStroke(0, new BasicStroke(2.0f));
+
+        plot.setRenderer(renderer);
+        plot.setBackgroundPaint(Color.white);
+
+        plot.setRangeGridlinesVisible(true);
+        plot.setRangeGridlinePaint(Color.BLACK);
+
+        plot.setDomainGridlinesVisible(true);
+        plot.setDomainGridlinePaint(Color.BLACK);
+
+        chart.getLegend().setFrame(BlockBorder.NONE);
+
+        chart.setTitle(new TextTitle("Average prix per id_event",
+                        new Font("Serif", java.awt.Font.BOLD, 18)
+                )
+        );
+
+        return chart;
+
+    }*/
+ 
+}
